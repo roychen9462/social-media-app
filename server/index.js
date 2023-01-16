6c93fdb6fb109a3fb9101cfaf9631ad355dc96a4
RoyChen <roy94621@gmail.com>
2023-01-16 00:51:58 -0800

    Complete server and Add navbar

--- server/index.js
+++ server/index.js
@@ -10,10 +10,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
-//import postRoutes from "./routes/posts.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
-//import { createPost } from "./controllers/posts.js";
+import { createPost } from "./controllers/posts.js";
+// import User from "./models/User.js";
+// import Post from "./models/Post.js";
+// import { users, posts } from "./data/index.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -43,14 +46,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* Routes with files */
-//app.post("/auth/register", upload.single("picture"), register);
-app.post("/auth/register", register);
-//app.post("/posts", verifyToken, upload.single("picture"), createPost);
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* Routes */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-//app.use("/posts", postRoutes);
+app.use("/posts", postRoutes);
 
 /* Mongoos Setup */
 const PORT = process.env.PORT || 6001;
@@ -61,5 +63,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // Insert fake data only once
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
